e060aa5e5b3c30d30e0ea9cf0afa2e9f597541bb
yattom <tsutomu.yasui@gmail.com>
2013-11-25 12:09:43 +0900

    閉区間に含まれる

--- tddbc/range.cpp
+++ tddbc/range.cpp
@@ -4,5 +4,5 @@
 
 int contained_in_closed_range(int lower_endpoint, int upper_endpoint, int subject)
 {
-	return 0;
+	return lower_endpoint <= subject && subject <= upper_endpoint;
 }
--- tddbc/range_test.cpp
+++ tddbc/range_test.cpp
@@ -6,3 +6,19 @@
 TEST(閉区間に含まれるか, 中間の場合) {
 	ASSERT_TRUE(contained_in_closed_range(3, 8, 5));
 }
+
+TEST(閉区間に含まれるか, 開始点の場合) {
+	ASSERT_TRUE(contained_in_closed_range(3, 8, 3));
+}
+
+TEST(閉区間に含まれるか, 終了点の場合) {
+	ASSERT_TRUE(contained_in_closed_range(3, 8, 8));
+}
+
+TEST(閉区間に含まれるか, 小さすぎる場合) {
+	ASSERT_FALSE(contained_in_closed_range(3, 8, 2));
+}
+
+TEST(閉区間に含まれるか, 大きすぎる場合) {
+	ASSERT_FALSE(contained_in_closed_range(3, 8, 9));
+}
